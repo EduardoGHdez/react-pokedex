e16088bc3ec8f819bbd6de0cb61c1d330ab39ff3
Eduardo <eduardo.garcia@magmalabs.io>
2019-01-31 19:50:08 -0600

    Add PokeList component
    We want to have a place to render a Pokemons list.
    This adds a section container with a scrolleable property, for
    overflow case.

---
+++ src/components/PokeList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles/PokeList.css'
+
+const PokeList = () => {
+  return (
+    <section className="poke-list">
+    </section>
+  )
+}
+
+export default PokeList;
